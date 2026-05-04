82b57cdcc358b7a530ffdd7be190cc687aa85a8d
Mazhaiyarasan108 <Mazhaiyarasan.Murugan@cognizant.com>
2026-05-04 19:55:46 +0530

    packages update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideCharts(withDefaultRegisterables())
-  ]
+    provideCharts(withDefaultRegisterables()),
+    provideStore(),
+    provideEffects(),
+  ],
 };
