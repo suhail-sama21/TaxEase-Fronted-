5a6ff8e9c65c49a322834801b8ecbe2959001682
suhail-sama21 <suhail.sama@cognizant.com>
2026-05-04 08:46:25 +0530

    "Implented ngrx store -> login and signup"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideHttpClient } from '@angular/common/http';
+
+// NgRx Imports
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { authFeature } from './stores/authStore/auth.features';
+import { AuthEffects } from './stores/authStore/auth.effect';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideCharts(withDefaultRegisterables())
+    provideHttpClient(), // Essential for making API calls!
+    
+    // Wire up NgRx Store and Effects
+    provideStore({
+      [authFeature.name]: authFeature.reducer
+    }),
+    provideEffects([AuthEffects]),
+    
+    // Optional but highly recommended: NgRx DevTools for debugging in Chrome
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
   ]
 };
