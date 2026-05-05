30f5adef565ca4278b87a31cc557dff12dabb98b
giridharang2-lang <Giridharan.G2@cognizant.com>
2026-05-05 09:32:51 +0530

    added password reset, and fetching profile data from store

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environment/environment';
-import { AuthResponse } from '../../interfaces/auth.interface';
+import { AuthResponse, User } from '../../interfaces/auth.interface';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -15,5 +15,13 @@ export class AuthService {
 
   signup(userData: any): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.apiUrl}/register`, userData);
+  }
+
+  updateProfile(userData: any): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/profile`, userData);
+  }
+
+  getProfile(email: string): Observable<User> {
+    return this.http.get<User>(`http://localhost:8081/api/users/username/${email}`);
   }
 }
--- src/app/stores/authStore/auth.action.ts
+++ src/app/stores/authStore/auth.action.ts
@@ -3,11 +3,19 @@ import { AuthResponse } from '../../interfaces/auth.interface';
 
 
 export const login = createAction('[Auth] Login', props<{ credentials: { email: string; password: string } }>());
-export const loginSuccess = createAction('[Auth] Login Success', props<{ response: AuthResponse }>());
+export const loginSuccess = createAction('[Auth] Login Success', props<{ response: AuthResponse; email: string }>());
 export const loginFailure = createAction('[Auth] Login Failure', props<{ error: string }>());
 
 export const signup = createAction('[Auth] Signup', props<{ userData: any }>());
 export const signupSuccess = createAction('[Auth] Signup Success', props<{ response: AuthResponse }>());
 export const signupFailure = createAction('[Auth] Signup Failure', props<{ error: string }>());
+
+export const getProfile = createAction('[Auth] Get Profile', props<{ email: string }>());
+export const getProfileSuccess = createAction('[Auth] Get Profile Success', props<{ user: any }>());
+export const getProfileFailure = createAction('[Auth] Get Profile Failure', props<{ error: string }>());
+
+export const updateProfile = createAction('[Auth] Update Profile', props<{ userData: any }>());
+export const updateProfileSuccess = createAction('[Auth] Update Profile Success', props<{ user: any }>());
+export const updateProfileFailure = createAction('[Auth] Update Profile Failure', props<{ error: string }>());
 
 export const logout = createAction('[Auth] Logout');
