eb3ce9120911d760950172264c580bbda9e57b59
Suprajith <Suprajith.JE@cognizant.com>
2026-05-06 15:59:46 +0530

    added payment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-
+ 
 // NgRx Imports
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
@@ -11,36 +11,20 @@ import { authFeature } from './stores/authStore/auth.features';
 import { AuthEffects } from './stores/authStore/auth.effect';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
-
+ 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-<<<<<<< HEAD
-    provideHttpClient(), // Essential for making API calls! (Ithu mattum pothum)
-
-    // Wire up NgRx Store and Effects
-    provideStore({
-      [authFeature.name]: authFeature.reducer
-    }),
-    provideEffects([AuthEffects]),
-
-    // Optional but highly recommended: NgRx DevTools for debugging in Chrome
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-    provideCharts(withDefaultRegisterables())
-    // Duplicate provideHttpClient()-ah remove panniyachu
-  ]
-};
-=======
     provideHttpClient(withInterceptors([authInterceptor])), // <-- Added HTTP Client
     provideCharts(withDefaultRegisterables()),
-
+ 
     // NgRx Global Providers
     provideStore({
       // Register your auth feature state here
       [authFeature.name]: authFeature.reducer,
     }),
     provideEffects([AuthEffects]), // <-- Added Effects
-
+ 
     // NgRx DevTools (Highly recommended for debugging)
     provideStoreDevtools({
       maxAge: 25, // Retains last 25 states
@@ -51,4 +35,3 @@ export const appConfig: ApplicationConfig = {
     }),
   ],
 };
->>>>>>> 9c009c9a8fb8d3e1c9acba6835cab7ce5c5187c5
