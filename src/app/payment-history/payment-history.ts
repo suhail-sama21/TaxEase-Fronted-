c8478b3233be2c939ff4492e4648601417f116f5
Mazhaiyarasan108 <Mazhaiyarasan.Murugan@cognizant.com>
2026-05-15 09:38:48 +0530

    changes maded

--- src/app/payment-history/payment-history.ts
+++ src/app/payment-history/payment-history.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { PaymentService } from '../service/payment.service';
+import { PaymentService } from '../core/services/payment.service';
 import { Store } from '@ngrx/store';
 import { selectUser } from '../stores/authStore/auth.features';
 
